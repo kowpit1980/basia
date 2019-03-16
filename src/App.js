74bba2b9a2a4111db1cf53033370c56e66645695
Piotr Kowalewski <3106339@TEB-GDANSK.PL>
2019-03-16 12:53:19 +0100

    new layout

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Button} from 'react-mdl';
+import {Layout} from 'react-mdl';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello World
-            <Button raised accent>Button</Button>
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+         <Layout style={{background: 'url(/owl.jpg) center / cover'}}>
+         
+         </Layout>
+
+
+
       </div>
     );
   }
